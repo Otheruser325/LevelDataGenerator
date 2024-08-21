954ab327571875e0586fb3c311408db782c4c9fb
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2024-08-21 14:33:18 +0100

    Update leveldata_mineshaft.js

--- leveldata_mineshaft.js
+++ leveldata_mineshaft.js
@@ -175,17 +175,15 @@ function generateLevels_mineshaft() {
     let levelsToGenerate = parseInt(document.getElementById('levelsToGenerateInput').value);
 
     let lastLevel = {
-        "0 Param data": {
-            "0 int Tier": currentTier,
-            "0 int Level": currentLevel - 1,
-            "0 double Cost": currentCost,
-            "0 int NumberOfWorkers": 1,
-            "0 double GainPerSecondPerWorker": currentGain,
-            "0 double CapacityPerWorker": currentCapacity,
-            "0 int WorkerWalkingSpeedPerSecond": 2,
-            "1 UInt8 BigUpdate": 0,
-            "0 double SuperCashReward": 0
-        }
+        "Tier": currentTier,
+        "Level": currentLevel - 1,
+        "Cost": currentCost,
+        "NumberOfWorkers": 1,
+        "GainPerSecondPerWorker": currentGain,
+        "CapacityPerWorker": currentCapacity,
+        "WorkerWalkingSpeedPerSecond": 2,
+        "BigUpdate": 0,
+        "SuperCashReward": 0
     };
 
     // Define super cash rewards based on tier ranges
@@ -202,32 +200,31 @@ function generateLevels_mineshaft() {
     for (let i = 0; i < levelsToGenerate; i++) {
         let newLevel = {};
 
-        newLevel["0 Param data"] = {};
-        newLevel["0 Param data"]["0 int Tier"] = currentTier;
-        newLevel["0 Param data"]["0 int Level"] = lastLevel["0 Param data"]["0 int Level"] + 1;
+        newLevel["Tier"] = currentTier;
+        newLevel["Level"] = lastLevel["Level"] + 1;
 
         // Determine the correct multiplier based on the level
         let currentCostMultiplier;
         let currentStatMultiplier;
-        if (newLevel["0 Param data"]["0 int Level"] < 21) {
+        if (newLevel["Level"] < 21) {
             currentCostMultiplier = costMultiplier;
             currentStatMultiplier = statMultiplier;
-        } else if (newLevel["0 Param data"]["0 int Level"] < 101) {
+        } else if (newLevel["Level"] < 101) {
             currentCostMultiplier = costMultiplier21;
             currentStatMultiplier = statMultiplier21;
-        } else if (newLevel["0 Param data"]["0 int Level"] < 401) {
+        } else if (newLevel["Level"] < 401) {
             currentCostMultiplier = costMultiplier101;
             currentStatMultiplier = statMultiplier101;
-        } else if (newLevel["0 Param data"]["0 int Level"] < 801) {
+        } else if (newLevel["Level"] < 801) {
             currentCostMultiplier = costMultiplier401;
             currentStatMultiplier = statMultiplier401;
-        } else if (newLevel["0 Param data"]["0 int Level"] < 851) {
+        } else if (newLevel["Level"] < 851) {
             currentCostMultiplier = costMultiplier801;
             currentStatMultiplier = statMultiplier801;
-        } else if (newLevel["0 Param data"]["0 int Level"] < 1001) {
+        } else if (newLevel["Level"] < 1001) {
             currentCostMultiplier = costMultiplier851;
             currentStatMultiplier = statMultiplier851;
-        } else if (newLevel["0 Param data"]["0 int Level"] < 1501) {
+        } else if (newLevel["Level"] < 1501) {
             currentCostMultiplier = costMultiplier1001;
             currentStatMultiplier = statMultiplier1001;
         } else {
@@ -235,57 +232,57 @@ function generateLevels_mineshaft() {
             currentStatMultiplier = statMultiplier1501;
         }
 
-        newLevel["0 Param data"]["0 double Cost"] = lastLevel["0 Param data"]["0 double Cost"] * currentCostMultiplier;
-        if (workerCountIncrementLevel[newLevel["0 Param data"]["0 int Level"]]) {
-            newLevel["0 Param data"]["0 int NumberOfWorkers"] = workerCountIncrementLevel[newLevel["0 Param data"]["0 int Level"]];
+        newLevel["Cost"] = lastLevel["Cost"] * currentCostMultiplier;
+        if (workerCountIncrementLevel[newLevel["Level"]]) {
+            newLevel["NumberOfWorkers"] = workerCountIncrementLevel[newLevel["Level"]];
         } else {
-            newLevel["0 Param data"]["0 int NumberOfWorkers"] = lastLevel["0 Param data"]["0 int NumberOfWorkers"];
+            newLevel["NumberOfWorkers"] = lastLevel["NumberOfWorkers"];
         }
-        newLevel["0 Param data"]["0 double GainPerSecondPerWorker"] = lastLevel["0 Param data"]["0 double GainPerSecondPerWorker"] * currentStatMultiplier;
-        newLevel["0 Param data"]["0 double CapacityPerWorker"] = lastLevel["0 Param data"]["0 double CapacityPerWorker"] * currentStatMultiplier;
-        if (workerSpeedIncrementLevel[newLevel["0 Param data"]["0 int Level"]]) {
-            newLevel["0 Param data"]["0 int WorkerWalkingSpeedPerSecond"] = workerSpeedIncrementLevel[newLevel["0 Param data"]["0 int Level"]];
+        newLevel["GainPerSecondPerWorker"] = lastLevel["GainPerSecondPerWorker"] * currentStatMultiplier;
+        newLevel["CapacityPerWorker"] = lastLevel["CapacityPerWorker"] * currentStatMultiplier;
+        if (workerSpeedIncrementLevel[newLevel["Level"]]) {
+            newLevel["WorkerWalkingSpeedPerSecond"] = workerSpeedIncrementLevel[newLevel["Level"]];
         } else {
-            newLevel["0 Param data"]["0 int WorkerWalkingSpeedPerSecond"] = lastLevel["0 Param data"]["0 int WorkerWalkingSpeedPerSecond"];
+            newLevel["WorkerWalkingSpeedPerSecond"] = lastLevel["WorkerWalkingSpeedPerSecond"];
         }
 
         // Apply big update and super cash rewards
         const bigUpdateLevels = [10, 25, 50, 100, 200, 400, 500, 600, 700, 800, 850, 900, 1000, 1200, 1400, 1500, 1600, 1750, 1875, 2000];
-        if (bigUpdateLevels.includes(newLevel["0 Param data"]["0 int Level"])) {
-            newLevel["0 Param data"]["1 UInt8 BigUpdate"] = 1;
+        if (bigUpdateLevels.includes(newLevel["Level"])) {
+            newLevel["BigUpdate"] = 1;
             let reward = 2;  // Default reward
             for (let tier in superCashRewards) {
                 if (currentTier >= tier) {
                     reward = superCashRewards[tier];
                 }
             }
-            newLevel["0 Param data"]["0 double SuperCashReward"] = reward;
+            newLevel["SuperCashReward"] = reward;
         } else {
-            newLevel["0 Param data"]["1 UInt8 BigUpdate"] = 0;
-            newLevel["0 Param data"]["0 double SuperCashReward"] = 0;
+            newLevel["BigUpdate"] = 0;
+            newLevel["SuperCashReward"] = 0;
         }
 
         // Apply special conditions for specific levels
-        if ([25, 50, 100, 200, 400, 500, 600, 700, 800].includes(newLevel["0 Param data"]["0 int Level"])) {
-            newLevel["0 Param data"]["0 double GainPerSecondPerWorker"] *= 2;
-            newLevel["0 Param data"]["0 double CapacityPerWorker"] *= 2;
-        } else if ([850, 900].includes(newLevel["0 Param data"]["0 int Level"])) {
-            newLevel["0 Param data"]["0 double GainPerSecondPerWorker"] *= 3;
-            newLevel["0 Param data"]["0 double CapacityPerWorker"] *= 3;
-        } else if ([1000, 1200, 1400].includes(newLevel["0 Param data"]["0 int Level"])) {
-            newLevel["0 Param data"]["0 double GainPerSecondPerWorker"] *= 4;
-            newLevel["0 Param data"]["0 double CapacityPerWorker"] *= 4;
-        } else if ([1500, 1600].includes(newLevel["0 Param data"]["0 int Level"])) {
-            newLevel["0 Param data"]["0 double GainPerSecondPerWorker"] *= 5;
-            newLevel["0 Param data"]["0 double CapacityPerWorker"] *= 5;
-        } else if ([1875].includes(newLevel["0 Param data"]["0 int Level"])) {
-            newLevel["0 Param data"]["0 double GainPerSecondPerWorker"] *= 7;
-            newLevel["0 Param data"]["0 double CapacityPerWorker"] *= 7;
-        } else if ([1750, 2000].includes(newLevel["0 Param data"]["0 int Level"])) {
-            newLevel["0 Param data"]["0 double GainPerSecondPerWorker"] *= 10;
-            newLevel["0 Param data"]["0 double CapacityPerWorker"] *= 10;
+        if ([25, 50, 100, 200, 400, 500, 600, 700, 800].includes(newLevel["Level"])) {
+            newLevel["GainPerSecondPerWorker"] *= 2;
+            newLevel["CapacityPerWorker"] *= 2;
+        } else if ([850, 900].includes(newLevel["Level"])) {
+            newLevel["GainPerSecondPerWorker"] *= 3;
+            newLevel["CapacityPerWorker"] *= 3;
+        } else if ([1000, 1200, 1400].includes(newLevel["Level"])) {
+            newLevel["GainPerSecondPerWorker"] *= 4;
+            newLevel["CapacityPerWorker"] *= 4;
+        } else if ([1500, 1600].includes(newLevel["Level"])) {
+            newLevel["GainPerSecondPerWorker"] *= 5;
+            newLevel["CapacityPerWorker"] *= 5;
+        } else if ([1875].includes(newLevel["Level"])) {
+            newLevel["GainPerSecondPerWorker"] *= 7;
+            newLevel["CapacityPerWorker"] *= 7;
+        } else if ([1750, 2000].includes(newLevel["Level"])) {
+            newLevel["GainPerSecondPerWorker"] *= 10;
+            newLevel["CapacityPerWorker"] *= 10;
         }
-        
+
         // Copy the generated level to the output
         levelData_mineshaft.push(newLevel);
         lastLevel = newLevel;
